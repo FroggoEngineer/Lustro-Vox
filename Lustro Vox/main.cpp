dd5d99e0c8cf2a12d43d4e12a7a4c4e4620c78e9
kevso311 <kevso311@student.liu.se>
2017-01-21 10:36:28 +0100

    Not a good render

--- Lustro Vox/main.cpp
+++ Lustro Vox/main.cpp
@@ -1,13 +1,23 @@
 #include <SFML/Graphics.hpp>
+#include <math.h>
 
+using namespace std;
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1, 1), "SFML works!", sf::Style::Fullscreen);
-	
-	sf::RectangleShape shape(sf::Vector2f(10,10));
+	sf::Vector2u size = window.getSize();
+
+	int width = 320;
+	int height = 180;
+
+
+	int pixel_width = size.x / width;
+	int pixel_height = size.y / height;
+
+	sf::RectangleShape shape(sf::Vector2f(pixel_width, pixel_height));
 
 	shape.setFillColor(sf::Color::Green);
-
+	sf::Clock time;
 	while (window.isOpen())
 	{
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
@@ -36,11 +46,20 @@ int main()
 
 
 		window.clear();
+		shape.setPosition(0, 0);
 		//Rendering
 		//--------------------------------------------------------------------
 
+		for (int i{ 0 }; i < width; i++)
+			for (int j{ 0 }; j < height; j++) {
+				int r = (int)round((sin(j)+1)*127);
+				int g = (int)round((cos(i)+1)*127);
+				int b = (int)round(time.getElapsedTime().asMilliseconds()%256);
 
-
+				shape.setFillColor(sf::Color(r, g, b));
+				shape.setPosition(i*pixel_width, j*pixel_height);
+			}
+				
 		window.draw(shape);
 
 

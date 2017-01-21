33c385fca2d3e334a07090a8155dcdd851424972
Henrik Henriksson <hx@hx.ax>
2017-01-21 12:10:44 +0100

    Add alpha values...

--- Lustro Vox/Colors.h
+++ Lustro Vox/Colors.h
@@ -11,21 +11,21 @@ sf::Uint32 reverse_byte_order(sf::Uint32 col) {
 }
 
 sf::Uint32 COLORS[16] = {
-reverse_byte_order(0x000000),
-reverse_byte_order(0xFFFFFF),
-reverse_byte_order(0x68372B),
-reverse_byte_order(0x70A4B2),
-reverse_byte_order(0x6F3D86),
-reverse_byte_order(0x588D43),
-reverse_byte_order(0x352879),
-reverse_byte_order(0xB8C76F),
-reverse_byte_order(0x6F4F25),
-reverse_byte_order(0x433900),
-reverse_byte_order(0x9A6759),
-reverse_byte_order(0x444444),
-reverse_byte_order(0x6C6C6C),
-reverse_byte_order(0x9AD284),
-reverse_byte_order(0x6C5EB5),
-reverse_byte_order(0x959595)
+reverse_byte_order(0x000000ff),
+reverse_byte_order(0xFFFFFFff),
+reverse_byte_order(0x68372Bff),
+reverse_byte_order(0x70A4B2ff),
+reverse_byte_order(0x6F3D86ff),
+reverse_byte_order(0x588D43ff),
+reverse_byte_order(0x352879ff),
+reverse_byte_order(0xB8C76Fff),
+reverse_byte_order(0x6F4F25ff),
+reverse_byte_order(0x433900ff),
+reverse_byte_order(0x9A6759ff),
+reverse_byte_order(0x444444ff),
+reverse_byte_order(0x6C6C6Cff),
+reverse_byte_order(0x9AD284ff),
+reverse_byte_order(0x6C5EB5ff),
+reverse_byte_order(0x95959ff5)
 };
 

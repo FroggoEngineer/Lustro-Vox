8f0bc9067312962ebf064284cc6f4fabe752bd74
kevso311 <kevso311@student.liu.se>
2017-01-21 13:07:51 +0100

    Starting physics

--- Lustro Vox/Physics.h
+++ Lustro Vox/Physics.h
@@ -1,1 +1,23 @@
 #pragma once
+#ifndef PHYSICS_H
+#define PHYSICS_H
+
+#include <SFML/Graphics.hpp>
+
+class Physics {
+
+public:
+	Physics() = default;
+	~Physics() = default;
+	sf::Uint8* getFrame() {};
+	void start();
+	void stop();
+private:
+	sf::Uint8* glob{ nullptr }; 
+	sf::Uint8* calcCanvas{ nullptr };
+	void update();
+
+};
+
+
+#endif
